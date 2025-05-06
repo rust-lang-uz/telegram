1b2bd9191c8c199dadd7adf77bcd9ee9d1fefd8d
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-06 18:16:14 +0500

    chore: better joined

--- src/functions/joined.rs
+++ src/functions/joined.rs
@@ -1,11 +1,23 @@
 use orzklv::telegram::{timer::Timer, topic::Topics};
 use teloxide::{prelude::*, types::*};
 
-static TEXT: &str = "<b>Salom bo'lajak Rustacean!</b>\n\n\
-Sizlarni bu guruhda ko'rib turganimizdan mamnunmiz. Bu guruh Rust dasturlash tiliga qaratilgan hisoblanib, \
-bu yerda ushbu til haqida gaplashish, savollar berish yoki o'z fikrlaringiz bilan bo'lishishingiz mumkin. \
-Hamda, agar siz ushbu dasturlash tilida butunlay yangi bo'lsangiz, /roadmap yordamida kerakli boshlang'ich \
-maslahatlar, va hamda /useful yordamoda foydali resurslar olishingiz mumkin.
+static TEXT: &str = "<b>Salom bo'lajak Rustacean!</b>
+
+Sizlarni bu guruhda ko'rib turganimizdan mamnunmiz. \
+Bu guruh Rust dasturlash tiliga qaratilgan hisoblanib, \
+bu yerda ushbu til haqida gaplashish, savollar berish \
+yoki o'z fikrlaringiz bilan bo'lishishingiz mumkin. \
+Hamda, agar siz ushbu dasturlash tilida butunlay yangi \
+bo'lsangiz, /roadmap yordamida kerakli boshlang'ich \
+maslahatlar, va hamda /useful yordamoda foydali resurslar \
+olishingiz mumkin.
+
+Agar siz bu yerlarga yordam axtarib kelgan bo'lsangiz, \
+<a href=\"https://t.me/rustlanguz/14749\">yordam kanali</a> \
+ga muroojat qilishingizni so'rab qolamiz. Hamda, qoidalarni \
+/rules buyrug'i orqali o'qib olish esingizdan chiqmasin. \
+Keyin yana adminlar jazolashsa hayron bo'lib yurmang!
+
 ";
 
 pub async fn trigger(bot: &Bot, msg: &Message) -> ResponseResult<()> {
